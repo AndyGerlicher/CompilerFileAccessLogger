208666c4c50d4904b2d93547378534793efffc5f
Andy Gerlicher <angerlic@microsoft.com>
2019-10-21 12:29:18 -0700

    Init

---
+++ CompilerFileAccessLogger/CompilerFileAccessLogger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Codex.Analysis.Managed;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.CodeAnalysis;
+
+namespace CompilerFileAccessLogger
+{
+    public class CompilerFileAccessLogger : Logger
+    {
+        public override void Initialize(IEventSource eventSource)
+        {
+            eventSource.MessageRaised += EventSourceOnMessageRaised;
+        }
+
+        private void EventSourceOnMessageRaised(object sender, BuildMessageEventArgs e)
+        {
+            if (e is TaskCommandLineEventArgs commandLine && commandLine.CommandLine.IndexOf("csc.exe", StringComparison.OrdinalIgnoreCase) >= 0)
+            {
+                //Debugger.Launch();
+                var cscPosition = commandLine.CommandLine.IndexOf("csc.exe");
+                var cscArgString = commandLine.CommandLine.Substring(cscPosition + 8);
+
+                var ci = new CompilerInvocation()
+                {
+                    CommandLine = cscArgString,
+                    Language = LanguageNames.CSharp,
+                    ProjectFile = commandLine.ProjectFile
+                };
+
+                var args = ci.GetParsedCommandLineArguments();
+            }
+        }
+
+        private void VisitFiles(CommandLineArguments args)
+        {
+            foreach (var file in args.SourceFiles)
+            {
+                VisitInput(file.Path);
+            }
+
+            VisitInput(args.AppConfigPath);
+            VisitInput(args.DocumentationPath);
+            VisitOutput(args.OutputFileName);
+            
+            // TODO: Add the rest
+        }
+
+        private void VisitOutput(string filePath)
+        {
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                using (File.OpenWrite(filePath)) { }
+            }
+        }
+
+        private void VisitInput(string filePath)
+        {
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                using (File.OpenRead(filePath)) { }
+            }
+            
+        }
+    }
+}
---
+++ CompilerFileAccessLogger/CompilerInvocation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Codex.Analysis.Managed
+{
+    public class CompilerInvocation
+    {
+        public string Language { get; internal set; }
+        public string CommandLine { get; internal set; }
+        public string[] CommandLineArguments { get; internal set; }
+        public string ProjectFile { get; internal set; }
+        public string ProjectDirectory => Path.GetDirectoryName(ProjectFile);
+
+        public CompilerInvocation(string commandLineArgs = null)
+        {
+            CommandLine = commandLineArgs;
+        }
+
+        public string[] GetCommandLineArguments()
+        {
+            return CommandLineArguments ?? CommandLineParser.SplitCommandLineIntoArguments(CommandLine, removeHashComments: false).ToArray();
+        }
+
+        public CommandLineArguments GetParsedCommandLineArguments()
+        {
+            var sdkDirectory = RuntimeEnvironment.GetRuntimeDirectory();
+            var args = GetCommandLineArguments();
+            CommandLineArguments arguments;
+            if (Language == LanguageNames.CSharp)
+            {
+                arguments = CSharpCommandLineParser.Default.Parse(args, ProjectDirectory, sdkDirectory);
+            }
+            else
+            {
+                throw new Exception("no vb");
+                //arguments = VisualBasicCommandLineParser.Default.Parse(args, ProjectDirectory, sdkDirectory);
+            }
+
+            return arguments;
+        }
+
+        public override string ToString()
+        {
+            var commandLine = CommandLine ?? string.Join(" ", CommandLineArguments ?? new string[0]);
+            return $"{ProjectFile} {((commandLine != null && commandLine.Length > 60) ? commandLine.Substring(0, 60) + "..." : commandLine)}";
+        }
+    }
+}
